e2533b637355708bc4e557286612c1ad114144d8
Brent Gauthier <cajundrummer@gmail.com>
2024-07-05 12:31:19 -0400

    Build function to register new users

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,31 @@
 const router = require('express').Router()
+const pool = require('../db')
 
-router.get('/', (req, res) => {
-    res.send('This is the auth router working properly.')
+// Test route
+// router.get('/', (req, res) => {
+//     res.send('This is the auth router working properly.')
+// })
+
+// REGISTER
+router.post('/register', async(req, res) => {
+    try {
+        const {username, email, password} = req.body
+        // const user = await pool.query('SELECT * FROM users WHERE email = $1', [email])
+
+        // if (user.rows.length !== 0) {
+        //     return res.status(401).send('User is already registered.')
+        // }
+
+        const newUser = await pool.query(
+            'INSERT INTO users (username, email, password) VALUES ($1, $2, $3)',
+            [username, email, password]
+        )
+
+        res.status(201).json(newUser)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server error')
+    }
 })
 
 module.exports = router
