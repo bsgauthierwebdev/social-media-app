02094cab993efe80f3d96f70eb22ed6c3a4b2adb
Brent Gauthier <cajundrummer@gmail.com>
2024-07-05 12:33:00 -0400

    Build function to get all users for test purposes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,23 @@
 const router = require('express').Router()
+const db = require('../db')
 
 // Build test route for Users routing
-router.get('/', (req, res) => {
-    res.send('This is the userRoutes function working properly.')
+// router.get('/', (req, res) => {
+//     res.send('This is the userRoutes function working properly.')
+// })
+
+// Get All Users Route
+router.get('/', async (req, res) => {
+    try {
+        const {rows} = await db.query('SELECT * FROM users')
+
+        return res.status(200).json({
+            success: true,
+            users: rows
+        })
+    } catch (err) {
+        console.log(err.message)
+    }
 })
 
 module.exports = router
