9650add569c175bc21440e1ed22d1b457f3867e2
Brent Gauthier <cajundrummer@gmail.com>
2024-07-14 17:03:27 -0400

    Create function to update username

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const db = require('../db')
+const bcrypt = require('bcryptjs')
 
 // Build test route for Users routing
 // router.get('/', (req, res) => {
@@ -19,5 +20,44 @@ router.get('/', async (req, res) => {
         console.log(err.message)
     }
 })
+
+// Get a User
+
+router.get('/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const user = await db.query(
+            'SELECT * FROM users WHERE user_id = $1',
+            [id]
+        )
+
+        res.status(200).json(user.rows[0])
+    } catch (err) {
+        console.log(err.message)
+    }
+})
+
+// Follow a User
+
+// Unfollow a User
+
+// Update User
+
+router.put('/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const {username} = req.body
+        const updateUser = await db.query(
+            "UPDATE users SET username = $1 WHERE user_id = $2",
+            [username, id]
+        )
+        const user = await db.query('SELECT * FROM users WHERE user_id = $1', [id])
+        return res.status(200).json(user.rows[0])
+    } catch (err) {
+        return res.status(400).json(err.message)
+    }
+})
+
+// Delete User
 
 module.exports = router
