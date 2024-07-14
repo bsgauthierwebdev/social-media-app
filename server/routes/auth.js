92483cbf86e6fb9cf6ceece986a34570099e9924
Brent Gauthier <cajundrummer@gmail.com>
2024-07-14 15:26:19 -0400

    Complete register and login functions

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const pool = require('../db')
+const {hash, compare} = require('bcryptjs')
 
 // Test route
 // router.get('/', (req, res) => {
@@ -8,9 +9,16 @@ const pool = require('../db')
 
 // REGISTER
 router.post('/register', async(req, res) => {
+    const {username, email, password} = req.body
     try {
-        const {username, email, password} = req.body
-        // const user = await pool.query('SELECT * FROM users WHERE email = $1', [email])
+        // Hash the password
+        // const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await hash(password, 10)
+
+        // const user = await pool.query(
+        //     'SELECT * FROM users WHERE username = $1 OR email = $2', 
+        //     [username, email]
+        // )
 
         // if (user.rows.length !== 0) {
         //     return res.status(401).send('User is already registered.')
@@ -18,13 +26,49 @@ router.post('/register', async(req, res) => {
 
         const newUser = await pool.query(
             'INSERT INTO users (username, email, password) VALUES ($1, $2, $3)',
-            [username, email, password]
+            [username, email, hashedPassword]
         )
 
         res.status(201).json(newUser)
-    } catch (error) {
-        console.error(error.message)
-        res.status(500).send('Server error')
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).json(err)
+    }
+})
+
+// LOGIN
+
+router.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body
+
+        const userQuery = 'SELECT * FROM users WHERE email = $1'
+        const userResult = await pool.query(userQuery, [email])
+
+        if (userResult.rows.length === 0) {
+            return res.status(404).json('Invalid credentials')
+        }
+
+        const user = userResult.rows[0]
+
+        // if (password !== user.password) {
+        //     return res.status(404).json('Invalid credentials')
+        // }
+
+        const validPassword = await compare(password, user.password)
+
+        // console.log(user)
+        // console.log(password, user.password, validPassword)
+
+        if (!validPassword) {
+            return res.status(404).json('Invalid credentials')
+        }
+
+        
+
+        res.status(200).json(user)
+    } catch (err) {
+        res.status(500).json(err)
     }
 })
 
