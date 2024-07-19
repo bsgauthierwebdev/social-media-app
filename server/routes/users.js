c1dc5f3db7eb1084d6b009cff820167241cd9d5c
Brent Gauthier <cajundrummer@gmail.com>
2024-07-19 16:15:17 -0400

    Update Get All Users & Update username

--- server/routes/users.js
+++ server/routes/users.js
@@ -10,7 +10,7 @@ const bcrypt = require('bcryptjs')
 // Get All Users Route
 router.get('/', async (req, res) => {
     try {
-        const {rows} = await db.query('SELECT * FROM users')
+        const {rows} = await db.query('SELECT * FROM users ORDER BY user_id ASC')
 
         return res.status(200).json({
             success: true,
@@ -46,17 +46,49 @@ router.get('/:id', async (req, res) => {
 router.put('/:id', async (req, res) => {
     try {
         const {id} = req.params
-        const {username} = req.body
-        const updateUser = await db.query(
-            "UPDATE users SET username = $1 WHERE user_id = $2",
-            [username, id]
+        const {username, userId} = req.body
+        const isAdmin = await db.query(
+            'SELECT is_admin FROM users WHERE user_id = $1', [userId]
         )
-        const user = await db.query('SELECT * FROM users WHERE user_id = $1', [id])
-        return res.status(200).json(user.rows[0])
+        // console.log(isAdmin.rows[0].is_admin)
+        
+        // console.log(user.rows[0].is_admin)
+        // console.log(`Req body = ${userId}, Req params = ${id}`)
+        // console.log(userId === id)
+
+        if (userId === id || isAdmin.rows[0].is_admin) {
+            const updateUser = await db.query(
+                'UPDATE users SET username = $1 WHERE user_id = $2',
+                [username, id]
+            )
+            const user = await db.query(
+                'SELECT * FROM users WHERE user_id = $1',
+                [id]
+            )
+            return res.status(200).json(user.rows[0])
+        } else {
+            return res.status(400).json('You cannot update another user')
+        }
+
     } catch (err) {
-        return res.status(400).json(err.message)
+        return res.status(500).json(err.message)
     }
 })
+
+// router.put('/:id', async (req, res) => {
+//     try {
+//         const {id} = req.params
+//         const {username} = req.body
+//         const updateUser = await db.query(
+//             "UPDATE users SET username = $1 WHERE user_id = $2",
+//             [username, id]
+//         )
+//         const user = await db.query('SELECT * FROM users WHERE user_id = $1', [id])
+//         return res.status(200).json(user.rows[0])
+//     } catch (err) {
+//         return res.status(400).json(err.message)
+//     }
+// })
 
 // Delete User
 
