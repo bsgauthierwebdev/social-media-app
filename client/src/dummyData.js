40aba79ad962900dbdf2cfc8c93ee18a796db462
Brent Gauthier <cajundrummer@gmail.com>
2024-09-02 16:13:19 -0400

    Create a dummy data file to test components

---
+++ client/src/dummyData.js
@@ -0,0 +1,145 @@
+export const Users = [
+    {
+        id: 1,
+        profilePicture: 'assets/person/1.jpeg',
+        username: 'Holly Golightly'
+    },
+    {
+        id: 2,
+        profilePicture: 'assets/person/2.jpeg',
+        username: 'Homer Sexual'
+    },
+    {
+        id: 3,
+        profilePicture: 'assets/person/3.jpeg',
+        username: 'Amanda Huggenkiss'
+    },
+    {
+        id: 4,
+        profilePicture: 'assets/person/4.jpeg',
+        username: 'Mike Hunt'
+    },
+    {
+        id: 5,
+        profilePicture: 'assets/person/5.jpeg',
+        username: 'Dean Winchester'
+    },
+    {
+        id: 6,
+        profilePicture: 'assets/person/6.jpeg',
+        username: 'Ho Nugget'
+    },
+    {
+        id: 7,
+        profilePicture: 'assets/person/7.jpeg',
+        username: 'Bart Simpson'
+    },
+    {
+        id: 8,
+        profilePicture: 'assets/person/8.jpeg',
+        username: 'Stewie Griffin'
+    },
+    {
+        id: 9,
+        profilePicture: 'assets/person/9.jpeg',
+        username: 'Verbal Kent'
+    },
+    {
+        id: 10,
+        profilePicture: 'assets/person/10.jpeg',
+        username: 'Han Solo'
+    }
+]
+
+export const Posts = [
+    {
+        id: 1,
+        content: 'The worst loneliness is to not be comfortable with yourself.',
+        photo: 'https://randomwordgenerator.com/img/picture-generator/57e9dd474952a414f1dc8460962e33791c3ad6e04e5074417d2e72d6944cc1_640.jpg',
+        date: '5 minutes ago',
+        userId: 1,
+        like: 32,
+        comment: 9
+    },
+    {
+        id: 2,
+        content: 'Don’t be the person who says yes with their mouth but no with their actions',
+        photo: 'https://randomwordgenerator.com/img/picture-generator/52e4d2464356af14f1dc8460962e33791c3ad6e04e5074417d2e72d6934cc5_640.jpg',
+        date: '52 minutes ago',
+        userId: 2,
+        like: 16,
+        comment: 12
+    },
+    {
+        id: 3,
+        content: 'Death does not concern us, because as long as we exist, death is not here. And when it does come, we no longer exist.',
+        photo: 'https://randomwordgenerator.com/img/picture-generator/57e8d34b4d57a914f1dc8460962e33791c3ad6e04e507440772872d3924bc4_640.jpg',
+        date: '14 minutes ago',
+        userId: 2,
+        like: 15,
+        comment: 6
+    },
+    {
+        id: 4,
+        content: '"That’s the problem with clarity,” responded the Avatar. “It often sounds insulting."',
+        photo: 'https://randomwordgenerator.com/img/picture-generator/5fe5dd434a57b10ff3d8992cc12c30771037dbf85254794e7d2b7fd09444_640.jpg',
+        date: '2 minutes ago',
+        userId: 4,
+        like: 8,
+        comment: 19
+    },
+    {
+        id: 5,
+        content: 'Happy work is best done by the man who takes his long-term plans somewhat lightly and works from moment to moment. It is only our daily bread that we are encouraged to ask for. The present is the only time in which any duty can be done or any grace received.',
+        photo: 'https://randomwordgenerator.com/img/picture-generator/52e6d2464f50ae14f1dc8460962e33791c3ad6e04e507440742a7ad69549cd_640.jpg',
+        date: '45 minutes ago',
+        userId: 5,
+        like: 3,
+        comment: 9
+    },
+    {
+        id: 6,
+        content: 'Be who you needed when you were younger.',
+        photo: '',
+        date: '4 minutes ago',
+        userId: 6,
+        like: 3,
+        comment: 2
+    },
+    {
+        id: 7,
+        content: 'Adapt what is useful, reject what is useless, and add what is specifically your own.',
+        photo: 'https://randomwordgenerator.com/img/picture-generator/55e6d2404c56a914f1dc8460962e33791c3ad6e04e507440702d79d19e45cc_640.jpg',
+        date: '32 minutes ago',
+        userId: 7,
+        like: 35,
+        comment: 14
+    },
+    {
+        id: 8,
+        content: 'The people who stick with their goals don’t let their emotions determine their actions. Top performers still find a way to show up, to work through the boredom, and to embrace the daily practice that is required to achieve their goals.',
+        photo: 'https://randomwordgenerator.com/img/picture-generator/51e0d3464d52b10ff3d8992cc12c30771037dbf85254794e70267cd39644_640.jpg',
+        date: '14 minutes ago',
+        userId: 8,
+        like: 58,
+        comment: 69
+    },
+    {
+        id: 9,
+        content: "Here's a rule I recommend. Never practice two vices at once.",
+        photo: 'https://randomwordgenerator.com/img/picture-generator/54e7d4474e5aa514f1dc8460962e33791c3ad6e04e507440702d79d2964bc3_640.jpg',
+        date: '8 minutes ago',
+        userId: 9,
+        like: 65,
+        comment: 59
+    },
+    {
+        id: 10,
+        content: 'Wealth is the power to choose. Financial wealth is the power to choose how to spend money. Social wealth is the power to choose who to hang out with. Time wealth is the power to choose how to spend your day. Mental wealth is the power to choose how to spend your attention.',
+        photo: 'https://randomwordgenerator.com/img/picture-generator/53e3d6434d55a914f1dc8460962e33791c3ad6e04e507440762e7ad3954fcd_640.jpg',
+        date: '7 minutes ago',
+        userId: 10,
+        like: 9,
+        comment: 39
+    },
+]
