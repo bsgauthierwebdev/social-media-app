1f3e0adc5ab798c1bd652b0a4ea4f5fdbbf3d1dd
Brent Gauthier <cajundrummer@gmail.com>
2024-09-02 16:15:06 -0400

    Import useState & set up state to monitor like and isLiked states

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,34 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
+import { Users } from '../../dummyData'
 import './post.css'
 
-const Post = () => {
+const Post = ({post}) => {
+    // console.log(post)
+    // const user = Users.filter(u => u.id === 1)
+
+    // console.log(user[0].username)
+    const [like, setLike] = useState(post.like)
+    const [isLiked, setIsLiked] = useState(false)
+
+    const likeHandler = () => {
+        setLike(isLiked ? like - 1 : like + 1)
+        setIsLiked(!isLiked)
+    }
+
   return (
     <div className="postContainer">
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
-                    <img src="/assets/person/1.jpeg" alt="" className="postProfileImg" />
-                    <span className="postUsername">User Name</span>
-                    <span className="postDate">5 minutes ago</span>
+                    <img 
+                        src = {Users.filter(u => u.id === post.userId)[0].profilePicture} 
+                        alt="" className="postProfileImg" 
+                    />
+                    <span className="postUsername">
+                        {Users.filter(u => u.id === post.userId)[0].username}
+                    </span>
+                    <span className="postDate">{post.date}</span>
                 </div>
                 <div className="postTopRight">
                     <FontAwesomeIcon icon = {faEllipsisVertical} />
                 </div>
             </div>
             <div className="postCenter">
-                <span className="postText">Hey, it's my first post!</span>
-                <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+                <span className="postText">{post?.content}</span>
+                <img src={post?.photo} alt="" className="postImg" />
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
-                    <img src="assets/like.png" alt="" className="likeIcon" />
-                    <img src="assets/heart.png" alt="" className="likeIcon" />
-                    <span className="postLikeCounter">32 people like it</span>
+                    <img 
+                        src="assets/like.png" 
+                        alt="" className="likeIcon"
+                        onClick = {likeHandler} 
+                    />
+                    <img 
+                        src="assets/heart.png" 
+                        alt="" className="likeIcon"
+                        onClick = {likeHandler} 
+                    />
+                    <span className="postLikeCounter">{like} people like it</span>
                 </div>
                 <div className="postBottomRight">
-                    <span className="postCommentText">9 comments</span>
+                    <span className="postCommentText">{post.comment} comments</span>
                 </div>
             </div>
         </div>
