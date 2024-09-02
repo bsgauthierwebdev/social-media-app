07cb61d5dbf8772f7b242ff51548e0819a3da6a1
Brent Gauthier <cajundrummer@gmail.com>
2024-09-01 22:11:47 -0400

    Import Topbar

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Topbar from '../../components/topbar/Topbar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Topbar />
+    </div>
   )
 }
 
