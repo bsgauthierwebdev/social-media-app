1bb1ad6f2216fa7f53d7be1734a6a05e0e1d3e5f
Brent Gauthier <cajundrummer@gmail.com>
2024-09-01 22:11:39 -0400

    Build Topbar component

---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './topbar.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faMagnifyingGlass, faUser, faComments, faBell} from '@fortawesome/free-solid-svg-icons'
+
+const Topbar = () => {
+  return (
+    <div className="topbarContainer">
+        <div className="topbarLeft">
+          <span className="logo">SocialMediaApp</span>
+        </div>
+        <div className="topbarCenter">
+          <div className="searchbar">
+            <FontAwesomeIcon icon={faMagnifyingGlass} className = 'searchIcon' />
+            <input placeholder = 'Search for friends, posts or videos' className="searchInput" />
+          </div>
+        </div>
+        <div className="topbarRight">
+          <div className="topbarLinks">
+            <span className="topbarLink">Home</span>
+            <span className="topbarLink">Timeline</span>
+          </div>
+          <div className="topbarIcons">
+            <div className="topbarIconItem">
+              <FontAwesomeIcon icon = {faUser} />
+              <span className="topbarIconBadge">1</span>
+            </div>
+            <div className="topbarIconItem">
+              <FontAwesomeIcon icon = {faComments} />
+              <span className="topbarIconBadge">2</span>
+            </div>
+            <div className="topbarIconItem">
+              <FontAwesomeIcon icon = {faBell} />
+              <span className="topbarIconBadge">1</span>
+            </div>
+          </div>
+          <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+        </div>
+    </div>
+  )
+}
+
+export default Topbar
