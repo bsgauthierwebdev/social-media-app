2cd6cbcef895a6eb538882276e90f4f786222f25
Brent Gauthier <cajundrummer@gmail.com>
2024-09-02 09:23:42 -0400

    Add Share & Post components to the Feed component

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './feed.css'
+import Share from '../share/Share'
+import Post from '../post/Post'
 
 const Feed = () => {
   return (
     <div className="feed">
-      Feed
+      <div className="feedWrapper">
+        <Share />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
     </div>
   )
 }
