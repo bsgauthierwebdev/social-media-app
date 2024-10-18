d04e03d2b6df19fc44bf5abe9b94a32ac5d3c7f7
Brent Gauthier <cajundrummer@gmail.com>
2024-10-17 20:44:05 -0400

    Add routing to App component

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,23 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Home />
-      {/* <Profile /> */}
-      {/* <Login /> */}
-      {/* <Register /> */}
+      <Router>
+        <Routes>
+          <Route exact path = '/' element = {<Home />} />
+          <Route path = '/login' element = {<Login />} />
+          <Route path = '/register' element = {<Register />} />
+          <Route path = '/profile/:username' element = {<Profile />} />
+        </Routes>
+      </Router>
     </div>    
   );
 }
