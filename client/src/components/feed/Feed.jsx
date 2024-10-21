116dbef5c42f554a6a28e5f97cef2918935a6ab8
Brent Gauthier <cajundrummer@gmail.com>
2024-10-20 20:53:06 -0400

    Update Feed component to pull data from the server, not dummy data

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import './feed.css'
 import Share from '../share/Share'
 import Post from '../post/Post'
-import { Posts } from '../../dummyData'
+// import { Posts } from '../../dummyData'
 
 const Feed = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get('posts/timeline/1')
+      // console.log(res)
+      setPosts(res.data)
+    }
+    fetchPosts()
+  }, [])
+
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {Posts.map(p => (
-          <Post key = {p.id} post = {p} />
+        {posts.map((p) => (
+          <Post key = {p.post_id} post = {p} />
         ))}
       </div>
     </div>
